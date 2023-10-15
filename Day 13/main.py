425fd7a02360a42622a947da6ec9980022722020
Mubeen Ul Hassan <114654660+Mubeen-Ul-Hassan@users.noreply.github.com>
2023-10-15 18:54:37 +0500

    Day 13 Project: Debugging FizzBuzz

---
+++ Day 13/main.py
@@ -0,0 +1,25 @@
+# Bugged
+
+# target = int(input())
+# for number in range(1, target + 1):
+#   if number % 3 == 0 or number % 5 == 0:
+#     print("FizzBuzz")
+#   if number % 3 == 0:
+#     print("Fizz")
+#   if number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print([number])
+
+# Debugged
+
+target = int(input())
+for number in range(1, target + 1):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
