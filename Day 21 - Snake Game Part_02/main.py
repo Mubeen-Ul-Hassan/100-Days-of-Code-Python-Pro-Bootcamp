70eccf77a61327ef3aaba732e5654f291c4858a9
Mubeen-Ul-Hassan <mubeenulhassan396@gmail.com>
2024-01-21 08:07:06 +0500

    Day 21 Project: Snake Game Part 02

--- Day 21 - Snake Game Part_02/main.py
+++ Day 21 - Snake Game Part_02/main.py
@@ -45,7 +45,7 @@ while game_is_on:
         scoreboard.game_over()
 
     # Detect collision with tail
-    for segment in snake.segments[1::]:
+    for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
