4d3af6d2e8878a95524fa166e525a7af52f9fe66
Mubeen-Ul-Hassan <mubeenulhassan396@gmail.com>
2024-02-28 10:21:57 +0500

    Day 23 Project: The Turtle Crossing Capstone Project

---
+++ Day 23 - The Turtle Crossing Capstone Project/main.py
@@ -0,0 +1,37 @@
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+import time
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkeypress(player.move_forward, "w")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 25:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_lvel()
+
+
+screen.exitonclick()
